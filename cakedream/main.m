c8ffa7cea90b19a50e39bf20162143e57243f25b
pushi <pushi_519200@qq.com>
2014-05-13 10:26:57 +0800

    Initial Commit

---
+++ cakedream/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  cakedream
+//
+//  Created by cui yansong on 14-5-13.
+//  Copyright (c) 2014年 pushi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
