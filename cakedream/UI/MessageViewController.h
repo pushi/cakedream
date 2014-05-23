525ba51661365b37c1d36977b13e465e562d0481
pushi <pushi_519200@qq.com>
2014-05-23 11:36:02 +0800

    new

---
+++ cakedream/UI/MessageViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessageViewController.h
+//  cakedream
+//
+//  Created by cui yansong on 14-5-23.
+//  Copyright (c) 2014年 pushi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageViewController : UIViewController
+
+@end
